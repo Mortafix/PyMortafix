9325f1b5359b47b14af29ff2ac2dd4d21463ae6b
Moris Doratiotto <moris.doratiotto@gmail.com>
2021-03-02 14:14:30 +0100

    Fix: conditions and add check parameters

--- pymortafix/utils.py
+++ pymortafix/utils.py
@@ -9,12 +9,18 @@ def multisub(sub_dict, string):
     return sub(rgx, lambda m: sub_dict.get(m.group()), string)
 
 
-def strict_input(text, wrong_text=None, choices=None, regex=None, flush=False):
+def strict_input(
+    text, wrong_text=None, choices=None, regex=None, flush=False, check=None
+):
     """Get user input with some requirements"""
     inp = input(text)
     if flush:
         erase(len(findall(r"\n", text)) + 1)
-    while (choices and inp not in choices) and (regex and not match(regex, inp)):
+    while (
+        (not choices or choices and inp not in choices)
+        and (not regex or regex and not match(regex, inp))
+        and (not check or check and not check(inp))
+    ):
         if wrong_text:
             inp = input(wrong_text)
         else:
