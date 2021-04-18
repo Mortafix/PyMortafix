86bf4b11c2bbcc38b3372c939208359b394d0d2b
Moris Doratiotto <moris.doratiotto@gmail.com>
2021-04-18 11:19:28 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymortafix",
-    version="0.1.6",
+    version="0.1.7",
     author="Moris Doratiotto",
     author_email="moris.doratiotto@gmail.com",
     description="A python module to collect some usefull stuff",
